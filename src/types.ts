6d02611a8fd9dd73b6b8895d6a2f3aa3e85439f2
pinky-pig <2545589993@qq.com>
2023-06-25 12:59:58 +0800

    feat: init PWA

---
+++ src/types.ts
@@ -0,0 +1,1 @@
+export type UserModule = (ctx: any) => void
--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import Vue from '@vitejs/plugin-vue'
 import Pages from 'vite-plugin-pages'
 import Components from 'unplugin-vue-components/vite'
 import AutoImport from 'unplugin-auto-import/vite'
+import { VitePWA } from 'vite-plugin-pwa'
 import UnoCSS from 'unocss/vite'
 import VueMacros from 'unplugin-vue-macros/vite'
 
@@ -54,6 +55,34 @@ export default defineConfig({
     // https://github.com/antfu/unocss
     // see uno.config.ts for config
     UnoCSS(),
+
+    // https://github.com/antfu/vite-plugin-pwa
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Arvin',
+        short_name: 'Playground',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: '/pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: '/pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+          {
+            src: '/pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable',
+          },
+        ],
+      },
+    }),
   ],
 
   // https://github.com/vitest-dev/vitest
